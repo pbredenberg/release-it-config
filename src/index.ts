69855e529fc177a3a39cfabfb578992372fe1989
Paul Bredenberg <paulbredenberg@gmail.com>
2020-03-18 15:08:55 -0400

    feat: move config to main entry file
    BREAKING: Consuming projects will need to change their property accessors
    in config files.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,35 @@
-import config from './config/release-it-config';
+export interface IRepositoryReleaseSettings {
+   release?: boolean;
+   releaseName?: string;
+   releaseNotes?: string;
+}
+
+const RELEASE_VERSION_NAME = 'release v${version}',
+      CHANGELOG_PATTERN: string | boolean = 'git log --pretty=format:"* %s (%h)" $(git describe --exclude "*rc*" --abbrev=0)...HEAD',
+      CHANGELOG_INFILE = 'CHANGELOG.md',
+      REPOSITORY_RELEASE_SETTINGS: IRepositoryReleaseSettings = {};
+
+REPOSITORY_RELEASE_SETTINGS.release = true;
+REPOSITORY_RELEASE_SETTINGS.releaseName = 'Release ${tagName}';
+REPOSITORY_RELEASE_SETTINGS.releaseNotes = CHANGELOG_PATTERN;
 
 export default {
-   plugins: config.plugins,
-   git: config.git,
-   gitHub: config.gitHub,
-   gitLab: config.gitLab,
+   plugins: {
+      '@release-it/conventional-changelog': {
+         preset: 'conventionalcommits',
+         infile: CHANGELOG_INFILE,
+      },
+      '@silvermine/release-it-config/dist/plugins/pause-for-changelog.js': {
+         infile: CHANGELOG_INFILE,
+      },
+   },
+   git: {
+      tag: true,
+      tagName: 'v${version}',
+      tagAnnotation: RELEASE_VERSION_NAME,
+      commitMessage: 'chore: ' + RELEASE_VERSION_NAME,
+      changelog: CHANGELOG_PATTERN,
+   },
+   gitHub: REPOSITORY_RELEASE_SETTINGS,
+   gitLab: REPOSITORY_RELEASE_SETTINGS,
 };
