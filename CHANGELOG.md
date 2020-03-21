1c58574844f1c1417fc62741cd2500dff8a11915
Paul Bredenberg <paulbredenberg@gmail.com>
2020-03-21 17:01:05 -0400

    chore: release v0.2.0-alpha.0

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+## 0.2.0-alpha.0 (2020-03-21)
+
+
+### Features
+
+* add cli tool ([1682296](https://github.com/silvermine/release-it-config/commit/1682296e2d728bac5153077d4765120a33ddf491))
+* add initial configurations ([52ff0a8](https://github.com/silvermine/release-it-config/commit/52ff0a8b0e7956005a2b897d4295b8c02678a1dc))
+* disable git push, tag, and npm publish by default ([15bc7ef](https://github.com/silvermine/release-it-config/commit/15bc7ef637ca27d90b94c9c15cc497be49a219b8))
+* move config to main entry file ([69855e5](https://github.com/silvermine/release-it-config/commit/69855e529fc177a3a39cfabfb578992372fe1989))
+* new git describe for changelogs ([bdbf2a9](https://github.com/silvermine/release-it-config/commit/bdbf2a906238efeedf1bdc5af7a8e3f226203b37))
+
--- src/cli.ts
+++ src/cli.ts
@@ -3,41 +3,81 @@ import releaseIt from 'release-it';
 import releaseItOptions from './index';
 
 const run = (): void => {
-   const args = process.argv || [];
+   const args = process.argv.slice(2) || [];
 
-   args.forEach((arg: string) => {
-      const argumentValues = arg.split('=');
+   console.log(args); // eslint-disable-line
 
-      console.log(args); // eslint-disable-line
+   const argumentValues = args.length > 1 ? args.join(' ').split('=') : [];
 
-      // TODO: Create interface for release-it options.
-      const runRelease = (options: object): void => {
-         let mergedOptions = Object.assign(releaseItOptions, options);
+   // TODO: Create interface for release-it options.
+   const runRelease = (options: object): void => {
+      let mergedOptions = Object.assign(releaseItOptions, options);
 
-         releaseIt(mergedOptions)
-            .then((output: any) => {
-               console.log('output', output); // eslint-disable-line
-            });
-      };
+      releaseIt(mergedOptions)
+         .then((output: any) => {
+            console.log('(silvermine-release) finished:', output); // eslint-disable-line
+         });
+   };
 
-      const preReleaseWhitelist = [
-         'alpha',
-         'beta',
-         'rc',
-      ];
+   const preReleaseWhitelist = [
+      'alpha',
+      'beta',
+      'rc',
+   ];
 
-      if (argumentValues[0] === '--preRelease') {
-         const preReleaseType = argumentValues[1];
+   const preReleaseArgIndex = argumentValues.indexOf('--preRelease');
 
-         if (preReleaseWhitelist.indexOf(preReleaseType) === -1) {
-            throw new Error(
-               `Please provide a valid pre-release type such as: ${preReleaseWhitelist.join(', ')}`
-            );
-         }
+   const npmReleaseArgIndex = argumentValues.indexOf('--release-npm');
 
-         runRelease({ preRelease: preReleaseType });
+   let userConfig: { [label: string]: any} = {};
+
+   console.log('silvermine-release:', argumentValues, preReleaseArgIndex); // eslint-disable-line
+
+   if (preReleaseArgIndex > -1) {
+      const preReleaseType = argumentValues[preReleaseArgIndex + 1];
+
+      if (preReleaseWhitelist.indexOf(preReleaseType) === -1) {
+         throw new Error(
+            `Please provide a valid pre-release type such as: ${preReleaseWhitelist.join(', ')}`
+         );
       }
-   });
+
+      userConfig.preRelease = preReleaseType;
+   }
+
+   if (npmReleaseArgIndex > -1) {
+      Object.assign(
+         userConfig,
+         {
+            increment: false,
+            plugins: [],
+            git: Object.assign(
+               releaseItOptions.git,
+               {
+                  push: true,
+                  tag: true,
+                  commit: false,
+                  changelog: false,
+               }
+            ),
+            npm: {
+               publish: true,
+            },
+         }
+      );
+   }
+
+   if (args[0] === '--help') {
+      // eslint-disable-next-line
+      console.log('(silvermine-release) Allowed commands: --preRelease, --release-npm');
+   } else if (argumentValues.length > 0) {
+      runRelease(userConfig);
+   } else {
+      // eslint-disable-next-line
+      console.log(
+         '(silvermine-release) Allowed commands: --preRelease, --release-npm, --help'
+      );
+   }
 };
 
 run();
